4514cae219495b7c4a9bf029aee1a05386f3744d
Jingna Li <jingna13@gmail.com>
2018-12-27 23:27:58 -0800

    Add solution for maxSubArray

---
+++ ds_algorithms/practice/maxSubArray.js
@@ -0,0 +1,8 @@
+var maxSubArray = function(nums) {
+  for ( let i = 0; i < nums.length; i++ ) {
+      if ( nums[i-1] > 0 ) {
+          nums[i] += nums[i-1];
+      }
+  }
+  return Math.max(...nums);
+};
